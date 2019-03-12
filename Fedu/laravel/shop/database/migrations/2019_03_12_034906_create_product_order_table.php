e25cf59118c6565559da3bce96ea179231b7b9c5
loinlt <loinguyenlamthanh@gmail.com>
2019-03-12 13:48:01 +0700

    update database with migrations

--- Fedu/laravel/shop/database/migrations/2019_03_12_034906_create_product_order_table.php
+++ Fedu/laravel/shop/database/migrations/2019_03_12_034906_create_product_order_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateProductOrderTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,10 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+        Schema::create('product_order', function (Blueprint $table) {
+            $table->integer('product_id')->unsigned();
+            $table->integer('order_id')->unsigned();
+            $table->integer('quantity')->default(1)->unsigned();
             $table->timestamps();
         });
     }
@@ -31,6 +28,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('product_order');
     }
 }
